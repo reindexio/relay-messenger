ca992d7f091239571c29f4ab2ca215e586786d8c
Johannes Stein <johannesstein@freeze-dev.com>
2016-06-01 14:32:09 +0200

    Add messenger with component state and mock data (#8)

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -3,32 +3,69 @@ import {
   Dimensions,
   View,
 } from 'react-native';
-import Relay from 'react-relay';
 import GiftedMessenger from 'react-native-gifted-messenger';
 
-class Channel extends Component {
+class Messages extends Component {
   static propTypes = {
     channel: PropTypes.object.isRequired,
     user: PropTypes.object.isRequired,
   };
-  render() {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentWillMount() {
     const { user } = this.props;
     const { edges } = this.props.channel.messages;
-    const messages = edges.map(({ node: { date, id, sender, text } }) => ({
+
+
+    const messages = edges.map(({ node: { date, id, sender, text } }) =>
+      this.constructMessage({ date, id, sender, text }, user));
+
+    this.setState({ messages });
+  }
+
+  constructMessage({ text, id = Date.now(), date = Date.now(), sender }, user) {
+    if (!sender) {
+      sender = user;
+    }
+
+    return {
       text,
       uniqueId: id,
       date: new Date(date),
       name: sender.name,
       image: { uri: sender.image },
       position: user.id === sender.id ? 'right' : 'left',
-    }));
+    };
+  }
+
+  handleSend = message => {
+    const { user } = this.props;
+    const currentMessages = this.state.messages;
+
+    this.setState({
+      messages: [...currentMessages, this.constructMessage({
+        text: message.text,
+      }, user)],
+    });
+  }
+
+  render() {
+    const { user } = this.props;
+
     return (
-      <View style={{
-        flex: 1,
-        marginTop: 20,
-      }}>
+      <View
+        style={{
+          flex: 1,
+          marginTop: 20,
+        }}>
         <GiftedMessenger
-          ref={(c) => { this._GiftedMessenger = c; }}
           styles={{
             container: {
               width: Dimensions.get('window').width,
@@ -39,8 +76,8 @@ class Channel extends Component {
             },
           }}
           autoFocus={false}
-          messages={messages}
-          handleSend={() => {}}
+          messages={this.state.messages}
+          handleSend={this.handleSend}
           onErrorButtonPress={() => {}}
           maxHeight={Dimensions.get('window').height - 20}
           loadEarlierMessagesButton={false}
@@ -59,34 +96,4 @@ class Channel extends Component {
   }
 }
 
-
-export default Relay.createContainer(Channel, {
-  fragments: {
-    user: () => Relay.QL`
-      fragment on User {
-        id
-        name
-        image
-      }
-    `,
-    channel: () => Relay.QL`
-      fragment on Channel {
-        name
-        messages {
-          edges {
-            node {
-              date
-              id
-              sender {
-                id
-                name
-                image
-              }
-              text
-            }
-          }
-        }
-      }
-    `,
-  },
-});
+export default Messages;
